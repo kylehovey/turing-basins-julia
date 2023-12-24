17e4040e7f478981bce176afe772160618417aa1
kylehovey <khoveyc@gmail.com>
2023-12-23 23:13:02 -0800

    Added scripts to generate static examples of rules

---
+++ saved/concat.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+# Define the directory containing your images. Adjust this to your image directory path.
+image_directory="."
+border_color="black"
+border_width=3
+
+# Initiate the ImageMagick convert command
+convert_cmd="convert"
+
+# Create an array to hold the sorted list of image files
+sorted_files=()
+while IFS= read -r file; do
+    sorted_files+=("$file")
+done < <(find "$image_directory" -maxdepth 1 -type f -name '*_step_*.png' | sort -t '_' -k 3,3n)
+
+# Check if there are any image files found
+if [[ ${#sorted_files[@]} -eq 0 ]]; then
+    echo "No image files found. Exiting."
+    exit 1
+fi
+
+# Extract the base prefix from the first sorted file for the output filename.
+# This strips away the numeric step part and the file extension, leaving the prefix.
+base_prefix=$(basename "${sorted_files[0]}" | sed -E 's/(_step_)[0-9]+(\.png)$//')
+output_image="${base_prefix}_concatenated.png"
+
+# Loop over the sorted files and append them to ImageMagick command
+for img in "${sorted_files[@]}"; do
+    convert_cmd="$convert_cmd \( \"$img\" -bordercolor $border_color -border ${border_width}x0 \) +append"
+done
+
+# Append the final output file to the convert command
+convert_cmd="$convert_cmd \"$image_directory/$output_image\""
+
+# Execute the command
+eval $convert_cmd
+
+for img in "${sorted_files[@]}"; do
+    rm $img
+done
+
+echo "Concatenated image saved as '$image_directory/$output_image'"
